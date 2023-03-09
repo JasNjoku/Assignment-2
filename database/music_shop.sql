279b398c2db9510c70a4bf4a8ba385446aa2529f
Jason Njoku <JasNjoku@gmail.com>
2023-03-09 12:44:21 +0000

    created database

--- database/music_shop.sql
+++ database/music_shop.sql
@@ -30,6 +30,53 @@ CREATE TABLE Albums (
   FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID)
 );
 
+INSERT INTO Artists (Name, Country) VALUES
+('Taylor Swift', 'USA'),
+('BTS', 'South Korea'),
+('Ariana Grande', 'USA'),
+('Ed Sheeran', 'UK'),
+('Dua Lipa', 'UK'),
+('Billie Eilish', 'USA'),
+('Drake', 'Canada'),
+('Lady Gaga', 'USA'),
+('The Weeknd', 'Canada');
+
+INSERT INTO Albums (Title, ArtistID, ReleaseYear) VALUES
+('folklore', 1, 2020),
+('BE', 2, 2020),
+('Positions', 3, 2020),
+('÷', 4, 2017),
+('Future Nostalgia', 5, 2020),
+('When We All Fall Asleep, Where Do We Go?', 6, 2019),
+('Views', 7, 2016),
+('Chromatica', 8, 2020),
+('Starboy', 9, 2016);
+
+INSERT INTO Songs (Title, ArtistID, Genre, ReleaseYear) VALUES
+('One Dance', 7, 'Hip-hop', 2016),
+('Stupid Love', 8, 'Dance-pop', 2020),
+('Starboy', 9, 'R&B', 2016),
+('Hotline Bling', 7, 'Hip-hop', 2015),
+('Rain on Me', 8, 'Dance-pop', 2020),
+('Can’t Feel My Face', 9, 'R&B', 2015),
+('Castle on the Hill', 4, 'Pop', 2017),
+('Perfect', 4, 'Pop', 2017),
+('Don''t Start Now', 5, 'Pop', 2019),
+('IDGAF', 5, 'Pop', 2018),
+('Ocean Eyes', 6, 'Alternative', 2016),
+('everything i wanted', 6, 'Alternative', 2019),
+('Shape of You', 4, 'Pop', 2017),
+('New Rules', 5, 'Pop', 2017),
+('bad guy', 6, 'Alternative', 2019),
+('Galway Girl', 4, 'Pop', 2017),
+('Levitating', 5, 'Disco', 2020),
+('bury a friend', 6, 'Alternative', 2019),
+('cardigan', 1, 'Indie Folk', 2020),
+('dynamite', 2, 'K-pop', 2020),
+('positions', 3, 'R&B', 2020),
+('willow', 1, 'Indie Folk', 2020),
+('Boy With Luv', 2, 'K-pop', 2019),
+('7 rings', 3, 'Pop', 2019);
 
 
 
